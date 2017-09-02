0230c8bf01f2016d00f620161000144274074613
amirb322 <amirb322@gmail.com>
2017-09-03 00:24:23 +0430

    final test

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Point p1 = new Point(10, 20);
-		Point p2 = new Point(10, 20);
-		System.out.println(p1.equals(p2));
+//		Point p1 = new Point(10, 20);
+//		Point p2 = new Point(10, 20);
+//		System.out.println(p1.equals(p2));
 		Login login = new Login();
 		login.main(null);
 	}
